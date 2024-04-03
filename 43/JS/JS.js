d40515b41f189c5f34fcb5d2e1788da773e2daf7
BUZZ1116 <110268621+BUZZ1116@users.noreply.github.com>
2024-04-03 12:46:49 +0800

    Update JS.js

--- 43/JS/JS.js
+++ 43/JS/JS.js
@@ -1,11 +1,20 @@
-document.write("<h1 style='background-color: lawngreen;text-shadow: 5px 5px 5px black;'>Hello World</h1>");
-let i = prompt("請輸入暱稱");
-if (i == null){
-    document.write("你好：訪客");
+document.write("<h1 style='background-color: lawngreen;text-shadow: 5px 5px 5px black;'<h1>BMI值計算器</h1>");
+let i = prompt("請輸入身高");
+let j = prompt("請輸入體重");
+let x =j/(i*i);
+if (i == null || j == null){
+    document.write("");
     document.close();
 }
 else{
-    document.write("你好："+ i);
+    alert("身高 "+i+" 公尺，體重"+j+"公斤你的BMI："+x+"");
+if (x <=18.5){
+    document.write("BMI體重過輕");
+}else if(x <=24){
+    document.write("BMI正常範圍");
+}else{
+document.write("BMI異常範圍");
+}
+   
     document.close(); 
 }
-
