44b90b88ffe87f01a2aea2db50085489d5d444b1
BUZZ1116 <110268621+BUZZ1116@users.noreply.github.com>
2024-09-02 15:58:42 +0800

    Add files via upload

--- 0503/navbar.js
+++ 0503/navbar.js
@@ -1,3 +1,4 @@
+// 创建导航栏
 function createNavBar() {
     const navBar = document.createElement('nav');
     navBar.className = 'navbar navbar-expand-lg navbar-light bg-light';
@@ -8,7 +9,8 @@ function createNavBar() {
     const brandLink = document.createElement('a');
     brandLink.className = 'navbar-brand';
     brandLink.href = 'index.html';
-
+    
+    // 添加品牌Logo和文本
     const logoImg = document.createElement('img');
     logoImg.src = 'logo.png';
     logoImg.alt = 'Logo';
@@ -16,16 +18,16 @@ function createNavBar() {
     logoImg.height = 30;
     logoImg.className = 'd-inline-block align-text-top';
     brandLink.appendChild(logoImg);
-
-    // 添加品牌文本和换行符
+    
+    // 添加品牌名称并换行
     const text1 = document.createTextNode('培英國民中學');
     const lineBreak = document.createElement('br');
     const text2 = document.createTextNode('112年生涯發展教育暨技藝教育');
-
     brandLink.appendChild(text1);
     brandLink.appendChild(lineBreak);
     brandLink.appendChild(text2);
 
+    // 创建菜单切换按钮
     const toggleButton = document.createElement('button');
     toggleButton.className = 'navbar-toggler';
     toggleButton.type = 'button';
@@ -38,6 +40,7 @@ function createNavBar() {
     toggleIcon.className = 'navbar-toggler-icon';
     toggleButton.appendChild(toggleIcon);
 
+    // 创建菜单项
     const collapseDiv = document.createElement('div');
     collapseDiv.className = 'collapse navbar-collapse';
     collapseDiv.id = 'navbarSupportedContent';
@@ -49,7 +52,7 @@ function createNavBar() {
         {
             name: '學校基本資料',
             submenu: [
-                { name: '全校班級數', modalId: 'classModal' }, // 触发班级数量模态窗口
+                { name: '全校班級數', modalId: 'classModal' }, // 触发全校班级数模态窗口
                 { name: '教師人數', modalId: 'teacherModal' }, // 触发教师人数模态窗口
                 { name: '辦理相關經費', modalId: 'fundingModal' }, // 触发经费模态窗口
                 { name: '技藝教育抽離式課程', modalId: 'skillCourseModal' }, // 触发技艺教育抽离式课程模态窗口
@@ -69,6 +72,7 @@ function createNavBar() {
         }
     ];
 
+    // 生成菜单项
     menuItems.forEach(item => {
         const li = document.createElement('li');
         li.className = item.submenu.length > 0 ? 'nav-item dropdown' : 'nav-item';
@@ -126,6 +130,7 @@ function createNavBar() {
     document.body.prepend(navBar);
 }
 
+// 创建页脚
 function createFooter() {
     const footer = document.createElement('footer');
     footer.className = 'text-center bg-light py-3';
@@ -133,189 +138,6 @@ function createFooter() {
     document.body.appendChild(footer);
 }
 
-function createModals() {
-    const modalsHtml = `
-    <!--全校班級數 Modal -->
-    <div class="modal fade" id="classModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="classModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="classModalLabel">全校班級數</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col">年級</th>
-                                <th scope="col">班級數</th>
-                                <th scope="col">學生人數</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>國中一年級（七年級）</td>
-                                <td>27</td>
-                                <td>801</td>
-                            </tr>
-                            <tr>
-                                <td>國中二年級（八年級）</td>
-                                <td>27</td>
-                                <td>779</td>
-                            </tr>
-                            <tr>
-                                <td>國中三年級（九年級）</td>
-                                <td>27</td>
-                                <td>803</td>
-                            </tr>
-                            <tr>
-                                <td>合計</td>
-                                <td>81</td>
-                                <td>2383</td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td colspan="3">
-                                    包含美術班、音樂班、體育班、舞蹈班、特教班等
-                                </td>
-                            </tr>
-                        </tfoot>
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--教師人數 Modal -->
-    <div class="modal fade" id="teacherModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="teacherModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="teacherModalLabel">教師人數</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col">類別</th>
-                                <th scope="col">教師人數（人）</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>員額編制</td>
-                                <td>195</td>
-                            </tr>
-                            <tr>
-                                <td>編制內正式教師</td>
-                                <td>174</td>
-                            </tr>
-                            <tr>
-                                <td>代理教師</td>
-                                <td>29</td>
-                            </tr>
-                            <tr>
-                                <td rowspan="2">任教輔導活動科教師人數</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>具輔導活動科合格教師證人數</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>不具輔導活動科合格教師證人數</td>
-                                <td>0</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!--辦理相關經費 Modal -->
-    <div class="modal fade" id="fundingModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="fundingModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="fundingModalLabel">辦理相關經費</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col">類別</th>
-                                <th scope="col">生涯發展教育</th>
-                                <th scope="col">抽離式技藝教育</th>
-                                <th scope="col">專業編班技藝教育</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>縣市政府補助（含教育部）</td>
-                                <td>344,000</td>
-                                <td>60,000</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <td>學校預算</td>
-                                <td>135,548</td>
-                                <td>0</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <td>自行籌措（如無，請填寫「0」）</td>
-                                <td>287,584</td>
-                                <td>0</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <td>合計</td>
-                                <td>767,132</td>
-                                <td>60,000</td>
-                                <td>0</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <!--技藝教育抽離式課程 Modal -->
-    <div class="modal fade" id="skillCourseModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="skillCourseModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="skillCourseModalLabel">技藝教育抽離式課程</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    技藝教育抽離式課程的內容將顯示在這裡。
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
-                </div>
-            </div>
-        </div>
-    </div>`;
-
-    document.body.insertAdjacentHTML('beforeend', modalsHtml);
-}
-
-// 呼叫函數來創建並插入導航欄、頁腳和模態視窗
+// 呼叫函数来创建并插入导航栏和页脚
 createNavBar();
 createFooter();
-createModals();
