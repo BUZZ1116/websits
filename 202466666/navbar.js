13e3e3a29942bbd06c746d7a1d0bd2c0123352db
BUZZ1116 <110268621+BUZZ1116@users.noreply.github.com>
2024-11-03 17:50:09 +0800

    Add files via upload

--- 202466666/navbar.js
+++ 202466666/navbar.js
@@ -49,26 +49,26 @@ function createNavBar() {
     const ul = document.createElement('ul');
     ul.className = 'navbar-nav me-auto mb-2 mb-lg-0';
 
-    // Adding "本日出勤表" menu item
+    // Adding "出勤表" menu item
     const li1 = document.createElement('li');
     li1.className = 'nav-item';
 
     const a1 = document.createElement('a');
     a1.className = 'nav-link';
     a1.href = '#';
-    a1.textContent = '本日出勤表';
+    a1.textContent = '出勤表';
 
     li1.appendChild(a1);
     ul.appendChild(li1);
 
-    // Adding "今日體檢表" menu item
+    // Adding "體檢表" menu item
     const li2 = document.createElement('li');
     li2.className = 'nav-item';
 
     const a2 = document.createElement('a');
     a2.className = 'nav-link';
     a2.href = '#';
-    a2.textContent = '今日體檢表';
+    a2.textContent = '體檢表';
 
     li2.appendChild(a2);
     ul.appendChild(li2);
